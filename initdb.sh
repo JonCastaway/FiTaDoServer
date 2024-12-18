d59f6dd45c5df42dae8e1cd931c9c0f68ca00431
Johnny Castaway <Johnny.Castaway@cybirdtech.com>
2024-12-18 15:16:24 +0000

    Update initdb.sh

--- initdb.sh
+++ initdb.sh
@@ -3,3 +3,4 @@ CREATE DATABASE IF NOT EXISTS syncstorage_rs;
 CREATE DATABASE IF NOT EXISTS tokenserver_rs;
 GRANT ALL PRIVILEGES ON syncstorage_rs.* TO 'sync'@'%';
 GRANT ALL PRIVILEGES ON tokenserver_rs.* TO 'sync'@'%';"
+//
